fe5b34341fcd6019a34ea82438940594b2ee0b36
Talon <talonpollard@gmail.com>
2020-02-09 19:39:05 -0800

    Complete project 4

--- tutorials.ino
+++ tutorials.ino
@@ -1,6 +1,64 @@
-  
-//////////////////// Project 4 ////////////////////
+//////////////////// Project 5 ////////////////////
 
+
+
+//////////////////// Project 4 ////////////////////
+  // const int redSensorPin = A0;
+  // const int greenSensorPin = A1;
+  // const int blueSensorPin = A2;
+
+  // const int redLEDPin = 11;
+  // const int greenLEDPin = 9;
+  // const int blueLEDPin = 10;
+
+  // int redValue = 0;
+  // int greenValue = 0;
+  // int blueValue = 0;
+
+  // int redSensorValue = 0;
+  // int greenSensorValue = 0;
+  // int blueSensorValue = 0;
+
+  // void setup() {
+  //   Serial.begin(9600);
+
+  //   pinMode(redLEDPin, OUTPUT);
+  //   pinMode(greenLEDPin, OUTPUT);
+  //   pinMode(blueLEDPin, OUTPUT);
+  // }
+
+  // void loop() {
+  //   redSensorValue = analogRead(redSensorPin);
+  //   delay(5);
+  //   greenSensorValue = analogRead(greenSensorPin);
+  //   delay(5);
+  //   blueSensorValue = analogRead(blueSensorPin);
+  //   delay(5);
+
+  //   Serial.print("Raw sensor values\t Red: ");
+  //   Serial.print(redSensorValue);
+  //   Serial.print("\t Green: ");
+  //   Serial.print(greenSensorValue);
+  //   Serial.print("\t Blue: ");
+  //   Serial.println(blueSensorValue);
+
+  //   redValue = redSensorValue / 4;
+  //   greenValue = greenSensorValue / 4;
+  //   blueValue = blueSensorValue / 4;
+
+  //   Serial.print("Mapped sensor values\t Red: ");
+  //   Serial.print(redValue);
+  //   Serial.print("\t Green: ");
+  //   Serial.print(greenValue);
+  //   Serial.print("\t Blue: ");
+  //   Serial.println(blueValue);
+
+  //   analogWrite(redLEDPin, redValue);
+  //   analogWrite(greenLEDPin, greenValue);
+  //   analogWrite(blueLEDPin, blueValue);
+
+  //   Serial.println();
+  // }
 
 //////////////////// Project 3 ////////////////////
   // const int sensorPin = A0;

4214c88a0c6abaad9f3ee76076ba3654aaacd6b8
Talon <talonpollard@gmail.com>
2020-02-12 21:22:12 -0800

    Update project 6

--- tutorials.ino
+++ tutorials.ino
@@ -1,6 +1,32 @@
 
 //////////////////// Project 6 ////////////////////
+int sensorValue;
+int sensorLow = 1023;
+int sensorHigh = 0;
 
+const int ledPin = 13;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  while (millis() < 5000) {
+    sensorValue = analogRead(A0);
+    if (sensorValue < sensorLow) {
+      sensorLow = sensorValue;
+    }
+    if (sensorValue > sensorHigh) {
+      sensorHigh = sensorValue;
+    }
+  }
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() {
+  snesorValue = analogRead(A0);
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+  tone(8, pitch, 20);
+  delay(10);
+}
 
 
 //////////////////// Project 5 ////////////////////
